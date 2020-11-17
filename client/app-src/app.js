cb677d1b24c6732569891c2aee3136d69b3f1493
Matheus Barbosa <matheusbrbs@gmail.com>
2020-11-17 11:15:13 -0300

    css files

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,7 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
@@ -9,10 +11,10 @@ const body = JSON.stringify(negociacao);
 const method = 'POST';
 
 const config = {
-    method,
-    headers,
-    body
+  method,
+  headers,
+  body
 };
 
 fetch('http://localhost:3000/negociacoes', config)
-    .then(() => console.log('Dado enviado com sucesso'));
+  .then(() => console.log('Dado enviado com sucesso'));
